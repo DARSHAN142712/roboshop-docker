e2187d793a71915d569ab71f826a78c2f60ff040
Darshan <ojaasghambheera@gmail.com>
2025-09-19 14:36:03 +0530

    docker start

--- mysql/db/schema.sql
+++ mysql/db/schema.sql
@@ -17,5 +17,4 @@ CREATE TABLE `cities` (
   KEY `region_idx` (`region`),
   KEY `c_code_idx` (`country_code`),
   FULLTEXT KEY `city_idx` (`city`)
-) ENGINE=InnoDB AUTO_INCREMENT=6223666 DEFAULT CHARSET=latin1;
-
+) ENGINE=InnoDB AUTO_INCREMENT=6223666 DEFAULT CHARSET=latin1;  
